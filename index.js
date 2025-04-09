7b4ed00deef9ad34d0e465f0100884d08e4b7c95
sushim123 <sushimpaddd@gmail.com>
2025-04-09 19:47:55 +0530

    added new origin in cors policy

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ dotenv.config();
 const app = express();
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["https://student-job-portal.vercel.app/", "http://localhost:5173"],
     credentials: true,
   })
 );
